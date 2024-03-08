ec8f4333bdbc70db1e2fd12c6605b6f368543954
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-03-07 23:35:33 -0300

    Show the youtube statistics

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,12 @@
+import { useState } from "preact/hooks";
+
 export function App() {
+  const [suscribers, setSuscribers] = useState(0);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Home</h1>
+      <h1 className="text-3xl font-bold underline">Youtbe Monitoring</h1>
+      <p>Suscribers: {suscribers}</p>
     </>
   );
 }
